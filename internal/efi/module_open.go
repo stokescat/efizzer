54c844416138d638a617b0589a5baf2b93445c9b
Pavel Naberezhnev <pavelnaberezhnev@gmail.com>
2026-02-26 01:53:18 +0300

    ADDED: OpenModuleByName() in efi package
    Signed-off-by: Pavel Naberezhnev <pavelnaberezhnev@gmail.com>

--- internal/efi/module_open.go
+++ internal/efi/module_open.go
@@ -24,3 +24,22 @@ func OpenModuleByGuid(dir string, g Guid) (*Module, error) {
 
   return obj, nil
 }
+
+func OpenModuleByName(dir, name string) (*Module, error) {
+
+  obj:= new(Module)
+  obj.Name = name
+
+  rawcovFileName:= name + ".rawcov"
+  rawcovFilePath:= filepath.Join(dir, rawcovFileName)
+
+  rawcovFile, err:= rawcov.Open(rawcovFilePath)
+  if err != nil {
+    return nil, err
+  }
+
+  obj.file = rawcovFile
+
+  return obj, nil
+
+}
